30fef6fc1ef85d89f4bbecd8cfce8b6cf9474453
Eyob <eyobeyasu145@gmail.com>
2025-11-12 08:41:21 +0300

    more feature about button

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lab1;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void disable(View v){
-        v.setEnabled(false);
+        Button button = (Button) v;
+        button.setText("Disabled");
         Log.d("succes","Button disable");
+    }
+    public void textCh(View view){
+        TextView tx = findViewById(R.id.texts);
+        tx.setText("Text Changed");
+
+
     }
 }
